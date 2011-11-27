e871460eb8acc8c63cf79e7d7bac71f600a70b67
Johan Attali <johan@MacBookPro.local>
2011-11-27 15:39:24 +0100

    added testing ios project

---
+++ code/src/os/ios/ttt/ttt/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  ttt
+//
+//  Created by Johan Attali on 11/27/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <GLKit/GLKit.h>
+
+@interface ViewController : GLKViewController
+
+@end
