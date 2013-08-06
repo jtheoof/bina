053324a25deae339a5c9e4cae585e0b73fbea902
Jeremy Attali <jeremy.attali@gmail.com>
2013-08-06 21:05:19 +0200

    android: these files should not be committed

--- projects/android/gen/R.java.d
+++
@@ -1,3 +0,0 @@
-/home/jeremy/dev/bina/src/android/gen/com/android/gl2jni/R.java \
- : /home/jeremy/dev/bina/src/android/res/values/strings.xml \
-/home/jeremy/dev/bina/src/android/bin/AndroidManifest.xml \
