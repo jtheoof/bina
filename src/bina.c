2fd6bcbfdc7585567d90f9fe348aa4b4c696b4c3
Jeremy Attali <jeremy.attali@gmail.com>
2013-08-08 23:44:05 +0200

    build: more strict build

--- src/bina.c
+++ src/bina.c
@@ -21,8 +21,7 @@
 void
 print_gl_string(const char* name, GLenum s)
 {
-    const char *v = (const char *) glGetString(s);
-    LOGI("GL %s = %s\n", name, v);
+    LOGI("GL %s = %s\n", name, glGetString(s));
 }
 
 /* static void */
